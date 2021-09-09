c9be3dd3d7df4d5580862b0da7b80fc03dc88c04
TarangGarlapally <taranggarlapally@gmail.com>
2021-09-09 14:23:54 +0530

    updated requirements

--- services.py
+++ services.py
@@ -43,15 +43,5 @@ def calculateScore(ipt):
 
 
 
-import requests
-data = {'email':'test@email.com',
-        'classes_': model.classes_.tolist() ,
-        'coef_':model.coef_.tolist() ,
-        'intercept_': model.intercept_.tolist() ,
-        'n_iter_': model.n_iter_.tolist()}
-
-resp = requests.post(url = "http://localhost:8000/get-score", json=data).json()
-print(float(resp))
-
 
 
