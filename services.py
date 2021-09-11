3377d7d765cee07aebae7ac45409a58eee670dc9
TarangGarlapally <taranggarlapally@gmail.com>
2021-09-11 20:03:20 +0530

    update post return to object

--- services.py
+++ services.py
@@ -44,6 +44,16 @@ def calculateScore(ipt):
 
 
 
+import requests
+data = {'email':'test@email.com',
+        'classes_': model.classes_.tolist() ,
+        'coef_':model.coef_.tolist() ,
+        'intercept_': model.intercept_.tolist() ,
+        'n_iter_': model.n_iter_.tolist()}
+
+resp = requests.post(url = "http://localhost:3000/send-model", json=data).json()
+print(resp)
+print(model.coef_.shape)
 
 
 
